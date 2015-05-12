c14cfd775f65c62ed590a008d5a9a188c26f1cd1
Kirsten Cassidy <mantismamita@hotmail.com>
2015-05-12 12:40:05 +0200

    Update Q6_program.js

--- Q6_program.js
+++ Q6_program.js
@@ -1,32 +1,16 @@
-// Export single function with 3 arguments (dirName, extString, callback)
-// 
-// Export a single function that takes exactly the arguments described.
-//   * Call the callback exactly once with an error or some data as described.
-//   * Don't change anything else, like global variables or stdout.
-//   * Handle all the errors that may occur and pass them to the callback.
-//   
-var fs = require('fs');
-var path = require('path');
+var mymodule = require('./mymodule.js');
+var dirName = process.argv[2];
+var extString = process.argv[3];
 
 
-module.exports = function(dirName, extString, callback) { 
+mymodule(dirName, extString, function(err, data){
 
-var fileList= new Array();
-var extString = '.' + extString;
-
-  fs.readdir(dirName, function(err, data){
-
-        if (err) {
-          return callback(err)
-        } 
-          
-        data.forEach(function(fileName){
-          
-            var currExt = path.extname(fileName);
-            if( currExt === extString){
-              fileList.push(fileName);
-            }
-        });
-        callback(null, fileList);
-  });
-};
+  if (err) {
+    throw err;  
+  } else {
+    
+    data.forEach(function(fileName){
+      console.log(fileName);
+    });
+  }
+});
